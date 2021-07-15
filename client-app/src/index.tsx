c5c81bcfcfe8a74dd328f704bf049b41553ebf3c
marko <marko@MARKO>
2021-07-15 18:12:33 +0200

    End of Section 8

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -5,6 +5,7 @@ import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -15,7 +16,9 @@ import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StoreContext.Provider>,
   document.getElementById('root')
 );

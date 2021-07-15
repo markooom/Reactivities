d974c13740d01b08c2ccd41a36e79da43a8043ae
marko <marko@MARKO>
2021-07-15 16:04:03 +0200

    End of Section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -13,7 +14,9 @@ import reportWebVitals from './reportWebVitals';
 // );
 
 ReactDOM.render(
-  <App />,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 

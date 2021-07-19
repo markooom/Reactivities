58fcf0c7b0bfa0bc46cf78bfe946674e9508c4df
marko <marko@MARKO>
2021-07-19 13:59:40 +0200

    End of Section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    { value: 'drinks', text: 'Drinks' },
+    { value: 'culture', text: 'Culture' },
+    { value: 'film', text: 'Film' },
+    { value: 'food', text: 'Food' },
+    { value: 'music', text: 'Music' },
+    { value: 'travel', text: 'Travel' },
+]
